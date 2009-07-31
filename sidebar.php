a569ef39c4e222c4c200ec0cfd10fcf6ae108b8c
Brandon Philips <brandon@ifup.org>
2009-07-31 02:13:03 +0000

    Add Qualcomm

--- sidebar.php
+++ sidebar.php
@@ -28,6 +28,7 @@
 				<h3><?php _e( 'Sponsors', 'sandbox' ) ?></h3>
 				<ul class="sponsor-links">
 					<li><a href="http://intel.com"><img src="/2009/style/intel_logo.png" /></a></li>
+					<li><a href="http://qualcomm.com"><img src="/2009/style/Qualcomm.png" /></a></li>
 					<li><a href="http://ibm.com"><img src="/2009/style/IBMtm.png" /></a></li>
 					<li><a href="http://hp.com"><img src="/2009/style/hp_logo.gif" /></a></li>
 					<li><a href="http://google.com"><img src="/2009/style/google_logo.gif" /></a></li>
